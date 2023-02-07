d2ec55827dc3acd9e9f6cb602129fa33028849ac
Noam Fadida <noam.official@gmail.com>
2023-02-07 16:50:53 +0200

    Simple exercise double a number

---
+++ chapter1/multipleByTwo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int number{};
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+    std::cout << "Double that number is: " << 2 * number << std::endl;
+}
